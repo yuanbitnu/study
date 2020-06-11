b20349d1aa639b08f45220eacdda214f73ddeb71
‘“0 <313510383@qq.com>
2020-06-11 22:19:15 +0800

    20200611宿舍_commit

--- SqlServer_project/smczUkeyManage/app/db/DBHelper.py
+++ SqlServer_project/smczUkeyManage/app/db/DBHelper.py
@@ -3,9 +3,11 @@ import os
 
 class DBHelper:
     
-    def __init__(self,host = '127.0.0.1',port = '1433',user = 'sa',password = 'Xxzx.5155932',database = 'UkeyDB'):
+    def __init__(self,host = '127.0.0.1',port = '1433',user = 'sa',password = 'Xxzx.5155932',database = 'UkeyDB',as_dicct = True):
         try:
             self.conn = pymssql.connect(host,user,password,database)
             self.cursor = self.conn.cursor()
-        except expression as e:
+        except Exception as e:
             print(e)
+            
+    def ExecQuery(self,sql)
