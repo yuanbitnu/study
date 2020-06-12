502549b86421ba8699d29fba484ef22e8a5a09ef
yuanbitnu <17711679006@189.cn>
2020-06-12 18:02:21 +0800

    20200612office_commit

--- SqlServer_project/smczUkeyManage/app/db/DBHelper.py
+++ SqlServer_project/smczUkeyManage/app/db/DBHelper.py
@@ -3,7 +3,7 @@ from DBUtils.PooledDB import PooledDB
 
 class DBHelper:
     
-    def __init__(self,host = host,port = port,user = user,password = password,database = database,as_dict = as_dict):
+    def __init__(self,host,port,user,password,database,as_dicta):
         try:
             self.Pool = PooledDB(creator = pymssql,mincached = 2,maxcached = 5,maxshared = 3,maxconnections = 6,blocking = True,\
                 host= host,port = port,user = user,password = password,database = database,charset = 'utf8')
