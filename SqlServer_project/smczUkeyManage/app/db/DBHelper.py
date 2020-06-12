1def186f849d3436d9da3a65d010245f09896505
‘“0 <313510383@qq.com>
2020-06-12 13:02:07 +0800

    20200612宿舍Commit

--- SqlServer_project/smczUkeyManage/app/db/DBHelper.py
+++ SqlServer_project/smczUkeyManage/app/db/DBHelper.py
@@ -1,13 +1,30 @@
 import pymssql
-import os
+from DBUtils.PooledDB import PooledDB
 
 class DBHelper:
     
-    def __init__(self,host = '127.0.0.1',port = '1433',user = 'sa',password = 'Xxzx.5155932',database = 'UkeyDB',as_dicct = True):
+    def __init__(self,host = host,port = port,user = user,password = password,database = database,as_dict = as_dict):
         try:
-            self.conn = pymssql.connect(host,user,password,database)
-            self.cursor = self.conn.cursor()
+            self.Pool = PooledDB(creator = pymssql,mincached = 2,maxcached = 5,maxshared = 3,maxconnections = 6,blocking = True,\
+                host= host,port = port,user = user,password = password,database = database,charset = 'utf8')
         except Exception as e:
             print(e)
             
-    def ExecQuery(self,sql)
+    def Getcursor(self):
+        try:
+            self.conn = self.Pool.connection()
+            cur = self.conn.cursor()
+            if not cur:
+                raise '数据库连接不上'
+            else:
+                return cur
+        except Exception as e:
+            print(e)
+            
+    def ExecQuery(self,sqlcom):
+        cur = self.Getcursor()
+        cur.execcut(sqlcom)
+        dic = cur.fetchall()
+        cur.close()       
+        self.conn.close()
+        return dic
